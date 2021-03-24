b804316dcf49190fc29e793336825b3ea81521b0
Jonathan Fine <jfine2358@gmail.com>
2021-03-24 16:42:52 +0000

    Minor cleanup, to improve before announcement.

--- finish/run-pdftex.sh
+++ finish/run-pdftex.sh
@@ -6,7 +6,7 @@ set -u
 TEXFORMATS=texlive/texmf-var/web2c/pdftex/ \
 	  TEXMFCNF=texlive/texmf-dist/web2c/ \
 	  strace -o story.strace \
-	  ./bin/pdftex story \\end
+	  ./bin/pdftex --batchmode story \\end
 
 
 cat story.strace | grep usr > story.usr.log
